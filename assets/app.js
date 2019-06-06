6452eb0129a08d470d44e273646aff0be3dce7e8
yutsukushi <ykushibi@gmail.com>
2019-06-06 18:43:12 -0400

    Set up and outlined initial files

---
+++ assets/app.js
@@ -0,0 +1,24 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDRMbft0VrvZ81zLTzM3BvuJ3z4uTWf9HQ",
+    authDomain: "yukie-76f54.firebaseapp.com",
+    databaseURL: "https://yukie-76f54.firebaseio.com",
+    projectId: "yukie-76f54",
+    storageBucket: "yukie-76f54.appspot.com",
+    messagingSenderId: "152968178176",
+    appId: "1:152968178176:web:89b83c89c1fe78ce"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+// * Code this app to calculate when the next train will arrive; this should be relative to the current time.
+
+// Create a variable to reference the database.
+// Initial Values
+// Capture Button Click
+// Code in the logic for storing and retrieving the most recent user.
+// Don't forget to provide initial data to your Firebase database.
+// Firebase watcher + initial loader HINT: .on("value")
+// Change the HTML to reflect
+// Handle the errors
